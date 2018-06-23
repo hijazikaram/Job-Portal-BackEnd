bd375f2b3c07b76dd6b7e3d0442c62bf3ab60107
Malik Hijazi <malik.hijazi@gmail.com>
2018-06-23 12:53:35 -0400

    ability to apply for job get applications by jobId or userId

--- model/application.js
+++ model/application.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
 const ApplicationSchema = mongoose.Schema({
-	name: {type: String, required: true},
+	userId: {type: String, required: true},
     email: { type: String, required: true }, 
     phoneNumber: { type: String, required: true }, 
     resumePath: { type: String, required: true },
-    jobId: { type: String, required: true }
+    jobId: { type: String, required: true },
+    created_at: { type: Date }
+});
+
+ApplicationSchema.pre('save', function(next) {
+    const currentDate = new Date();
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
 });
 
 module.exports = mongoose.model('Application', ApplicationSchema);
